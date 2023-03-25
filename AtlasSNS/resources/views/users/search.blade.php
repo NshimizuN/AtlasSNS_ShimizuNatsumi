fabbdf44d1f5e4c85d2e82f56d4af2999e250c78
NshimizuN <lolipopgirl723@gmail.com>
2023-03-25 23:07:30 +0900

    no message

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -8,14 +8,14 @@
 
   <!--検索機能-->
   <div class="top-container">
-    <div class="search-container">
+    <div class="search-box">
       <form action="/search" method="GET">
         @csrf
         <input type="text" class="search-form" name="keyword" value="{{ $keyword }}" placeholder="ユーザー名">
         <button type="submit" class="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
         <div class="search-word">
           @if(isset( $keyword ))
-          <p class="word">検索ワード：{{$keyword}}</p>
+          <div class="word">検索ワード：{{$keyword}}</div>
           @else
           @endif
         </div>
@@ -30,30 +30,32 @@
 
       @foreach($users as $user)
 
-      <div class="search-icon">
-        @if($user->images == "dawn.png")
-        <img src="/images/icon1.png" width="70" height="70">
-        @else
-        <img src=" {{ asset('storage/'.$user->images)}}" width="70" height="70">
-        @endif
-      </div>
+      <div class="user-list">
+        <div class="search-icon">
+          @if($user->images == "dawn.png")
+          <img src="/images/icon1.png" width="70" height="70">
+          @else
+          <img src=" {{ asset('storage/'.$user->images)}}" width="70" height="70">
+          @endif
+        </div>
 
-      <div class="search-name">
-        {{$user -> username}}
-      </div>
+        <div class="search-name">
+          {{$user -> username}}
+        </div>
 
-      <div class="followbtn-box">
-        @if(Auth::id() != $user->id)
-        <ul class="follows-btn">
-          <li>
-            @if (auth()->user()->isFollowing($user->id))
-            <p class="unfollow-btn"><a href="/search/{{$user->id}}/unfollow">フォロー解除</a>
-              @else
-            <p class="follow-btn"><a href="/search/{{$user->id}}/follow">フォローする</a></p>
-            @endif
-          </li>
-        </ul>
-        @endif
+        <div class="followbtn-box">
+          @if(Auth::id() != $user->id)
+          <ul class="follows-btn">
+            <li>
+              @if (auth()->user()->isFollowing($user->id))
+              <p class="unfollow-btn"><a href="/search/{{$user->id}}/unfollow">フォロー解除</a>
+                @else
+              <p class="follow-btn"><a href="/search/{{$user->id}}/follow">フォローする</a></p>
+              @endif
+            </li>
+          </ul>
+          @endif
+        </div>
       </div>
       @endforeach
     </div>
