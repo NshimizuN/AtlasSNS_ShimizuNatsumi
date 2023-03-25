fb94b1c26f8416305108303857bf32b535891482
NshimizuN <lolipopgirl723@gmail.com>
2023-03-25 15:19:25 +0900

    no message

--- AtlasSNS/resources/views/users/profile.blade.php
+++ AtlasSNS/resources/views/users/profile.blade.php
@@ -9,54 +9,54 @@
     <li>{{$error}}</li>
     @endforeach
 
-    <!--アイコン-->
-    <div class="edit-icon">
+    <div class="edit-box">
+
+      <!--アイコン-->
       @if(Auth::user()->images == "dawn.png")
       <img src="/images/icon1.png" width="70" height="70">
       @else
       <img src=" {{ asset('storage/'.Auth::user()->images)}}" width="70" height="70">
       @endif
+
+      <div class="edit-list">
+        <!--入力フォーム-->
+
+        <div class="ct-block">
+          <label class="contact-text" class="name">{{ Form::label('username') }}</label>
+          {{ Form::text('username',Auth::user()->username,['class' => 'input-form']) }}
+        </div>
+
+        <div class="ct-block">
+          <label class="contact-text" for="name">{{ Form::label('mail address') }}</label>
+          {{ Form::text('mail',Auth::user()->mail ,['class' => 'input-form']) }}
+        </div>
+
+        <div class="ct-block">
+          <label class="contact-text" for="name"> {{ Form::label('password') }}</label>
+          {{ Form::password('password',['class' => 'input-form']) }}
+        </div>
+
+        <div class="ct-block">
+          <label class="contact-text" for="name">{{ Form::label('password comfirm') }}</label>
+          {{ Form::password('password_confirmation',['class' => 'input-form']) }}
+        </div>
+
+        <div class="ct-block">
+          <label class="contact-text" for="name">{{ Form::label('bio') }}</label>
+          {{ Form::text('bio',Auth::user()->bio,['class' => 'input-form']) }}
+        </div>
+
+        <div class="ct-block">
+          <label class="contact-text" for="images">{{ Form::label('icon image') }}</label>
+          {{ Form::file('imgpath',['class' => 'input-form']) }}
+        </div>
+
+        <div class="update-box">
+          {{ Form::submit('更新',['class' => 'update-btn']) }}
+        </div>
+
+      </div>
     </div>
-
-    <div class="edit-list">
-      <!--入力フォーム-->
-
-      <div class="ct-block">
-        <label class="contact-text" class="name">{{ Form::label('username') }}</label>
-        {{ Form::text('username',Auth::user()->username,['class' => 'input-form']) }}
-      </div>
-
-      <div class="ct-block">
-        <label class="contact-text" for="name">{{ Form::label('mail address') }}</label>
-        {{ Form::text('mail',Auth::user()->mail ,['class' => 'input-form']) }}
-      </div>
-
-      <div class="ct-block">
-        <label class="contact-text" for="name"> {{ Form::label('password') }}</label>
-        {{ Form::password('password',['class' => 'input-form']) }}
-      </div>
-
-      <div class="ct-block">
-        <label class="contact-text" for="name">{{ Form::label('password comfirm') }}</label>
-        {{ Form::password('password_confirmation',['class' => 'input-form']) }}
-      </div>
-
-      <div class="ct-block">
-        <label class="contact-text" for="name">{{ Form::label('bio') }}</label>
-        {{ Form::text('bio',Auth::user()->bio,['class' => 'input-form']) }}
-      </div>
-
-      <div class="ct-block">
-        <label class="contact-text" for="images">{{ Form::label('icon image') }}</label>
-        {{ Form::file('imgpath',['class' => 'input-form']) }}
-      </div>
-
-      <div class="update-box">
-        {{ Form::submit('更新',['class' => 'update-btn']) }}
-      </div>
-
-    </div>
-  </form>
 
 </div>
 
